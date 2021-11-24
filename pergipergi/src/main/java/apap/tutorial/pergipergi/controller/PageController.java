eef16033e77092b70c75d604ae28773a1b1fe009
erika erika <90308304+erikahanaa@users.noreply.github.com>
2021-11-24 14:13:29 +0700

    Feat/tutorial 6 pergipergi (#13)
    * feat: tutorial 6 pergi pergi - web security
    
    * Update README.md
    
    * Update README.md

---
+++ pergipergi/src/main/java/apap/tutorial/pergipergi/controller/PageController.java
@@ -0,0 +1,40 @@
+package apap.tutorial.pergipergi.controller;
+
+import apap.tutorial.pergipergi.model.UserModel;
+import apap.tutorial.pergipergi.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class PageController {
+    @Autowired
+    private UserService userService;
+    
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/")
+    public String home(Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) auth.getPrincipal();
+        String username = user.getUsername();
+        UserModel userModel = userService.findByUsername(username);
+        model.addAttribute("user", userModel);
+        return "home";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
